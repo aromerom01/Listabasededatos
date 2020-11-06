04cae9bb8f726652e7d1da2677473c4e852b8fb2
“jhonatanbc <zjhonatan7@gmail.com>
2020-11-05 21:23:50 -0500

    conexion

--- src/listas/Listas.java
+++ src/listas/Listas.java
@@ -15,7 +15,8 @@ public class Listas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        conexion con = new conexion();
+        con.conectar();
     }
     
 }
